7da30bb9f193e6a18e2b9df949d33ccaf16adfa2
Kevin Obee <kevin.obee@gmail.com>
2013-08-20 19:02:51 +0700

    ITab test added

---
+++ src/Sitecore.Glimpse/SitecoreTab.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Glimpse.Core.Extensibility;
+
+namespace Sitecore.Glimpse
+{
+    public class SitecoreTab : ITab
+    {
+        public object GetData(ITabContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Name { get; private set; }
+        public RuntimeEvent ExecuteOn { get; private set; }
+        public Type RequestContextType { get; private set; }
+    }
+}
---
+++ tests/unit-test/Sitecore.Glimpse.Test/SitecoreGlimpseShould.cs
@@ -0,0 +1,17 @@
+﻿using Glimpse.Core.Extensibility;
+
+using Xunit;
+
+namespace Sitecore.Glimpse.Test
+{
+    public class SitecoreGlimpseShould
+    {
+        [Fact]
+        public void Provide_a_sitecore_tab()
+        {
+            var sut = new SitecoreTab();
+
+            Assert.IsAssignableFrom<ITab>(sut);
+        }
+    }
+}
