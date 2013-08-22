23f5c3bd21323b9fbc0a77beba0e99ed257983ce
Kevin Obee <kevin.obee@gmail.com>
2013-08-22 14:33:34 +0700

    seperate sitecore data retrieval from glimpse tab

--- src/Sitecore.Glimpse/SitecoreTab.cs
+++ src/Sitecore.Glimpse/SitecoreTab.cs
@@ -4,20 +4,67 @@ using Glimpse.Core.Extensibility;
 
 namespace Sitecore.Glimpse
 {
-    public class SitecoreTab : ITab
+    public class SitecoreTab : TabBase
     {
+        private readonly ISitecoreRequest _sitecoreRequest;
+
         public SitecoreTab()
+            : this(new FakeSitecoreRequest())
         {
-            Name = "Sitecore";
+
         }
 
-        public object GetData(ITabContext context)
+        public SitecoreTab(ISitecoreRequest sitecoreRequest)
+        {
+            _sitecoreRequest = sitecoreRequest;
+        }
+
+        public override object GetData(ITabContext context)
+        {
+            try
+            {
+                var stopWatch = new System.Diagnostics.Stopwatch();
+                stopWatch.Start();
+
+                return new object[] { 
+                                new object[] { "Sitecore.Glimpse" },
+                                new object[] {  _sitecoreRequest.GetData() },
+                                new object[] {  string.Format("Execution Time: {0}ms", stopWatch.ElapsedMilliseconds) } 
+                            };
+            }
+            catch (Exception ex)
+            {
+                return new object[] { 
+                                new object[] { "Exception" },
+                                new object[] { ex } 
+                            };
+            }
+        }
+
+        public override string Name
+        {
+            get { return "Sitecore"; }
+        }
+    }
+
+    public interface ISitecoreRequest
+    {
+        object[] GetData();
+    }
+
+    public class FakeSitecoreRequest : ISitecoreRequest
+    {
+        public object[] GetData()
+        {
+            return new object[] { "foo" };
+        }
+    }
+
+    public class SitecoreRequest : ISitecoreRequest
+    {
+        public object[] GetData()
         {
             throw new NotImplementedException();
         }
-
-        public string Name { get; private set; }
-        public RuntimeEvent ExecuteOn { get; private set; }
-        public Type RequestContextType { get; private set; }
     }
 }
--- tests/unit-test/Sitecore.Glimpse.Test/SitecoreGlimpseShould.cs
+++ tests/unit-test/Sitecore.Glimpse.Test/SitecoreGlimpseShould.cs
@@ -1,5 +1,6 @@
-﻿using Glimpse.Core.Extensibility;
-
+﻿using System.Linq;
+using Glimpse.Core.Extensibility;
+using Newtonsoft.Json.Linq;
 using Xunit;
 
 namespace Sitecore.Glimpse.Test
@@ -23,6 +24,30 @@ namespace Sitecore.Glimpse.Test
         public void Name_tab_Sitecore()
         {
             Assert.Equal("Sitecore", _sut.Name);
+        }
+
+        [Fact]
+        public void Returns_a_value_from_GetData()
+        {
+            var data = _sut.GetData(null);
+
+            Assert.NotNull(data);
+        }
+
+        [Fact]
+        public void Returns_an_object_array_value_from_GetData()
+        {
+            var data = _sut.GetData(null);
+
+            Assert.IsAssignableFrom<object[]>(data);
+        }
+
+        [Fact]
+        public void Return_Sitecore_information_in_returned_object_from_GetData()
+        {
+            dynamic data = _sut.GetData(null);
+
+            Assert.True(data[1][0][0].Contains("foo"));
         }
     }
 }
