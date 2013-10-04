9dff8b801381e5929f0916632e99b06cc2b6ede4
Kevin Obee <kevin.obee@gmail.com>
2013-10-04 16:38:44 +0100

    Issue #14, add support for net35 to sitecore.glimpse package

---
+++ src/Sitecore.Glimpse.Net35/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Sitecore.Glimpse.Net35")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyProduct("Sitecore.Glimpse.Net35")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("82d43b56-b665-428a-a3ad-ab36f296950a")]
