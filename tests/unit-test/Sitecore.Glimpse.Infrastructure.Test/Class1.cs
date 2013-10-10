01cc89da6765cafa1ce125b0731d4fbda6a03d79
Kevin Obee <kevin.obee@gmail.com>
2013-10-10 14:32:07 +0100

    Issue #15 - fix to show clones info in Sitecore 7 sites

---
+++ tests/unit-test/Sitecore.Glimpse.Infrastructure.Test/Class1.cs
@@ -0,0 +1,34 @@
+﻿using Xunit;
+
+namespace Sitecore.Glimpse.Infrastructure.Test
+{
+    public class FeaturesSupportedShould
+    {
+        [Fact]
+        public void Show_clones_not_supported_for_version_6_2()
+        {
+            var productVersion = new ProductVersion { MajorPart = 6, MinorPart = 2 };
+            FeaturesSupported.Version = productVersion;
+
+            Assert.False(FeaturesSupported.Clones);
+        }
+
+        [Fact]
+        public void Show_clones_supported_for_version_6_3()
+        {
+            var productVersion = new ProductVersion { MajorPart = 6, MinorPart = 3 };
+            FeaturesSupported.Version = productVersion;
+
+            Assert.True(FeaturesSupported.Clones);
+        }
+
+        [Fact]
+        public void Show_clones_supported_for_version_7_0()
+        {
+            var productVersion = new ProductVersion { MajorPart = 7, MinorPart = 0 };
+            FeaturesSupported.Version = productVersion;
+
+            Assert.True(FeaturesSupported.Clones);
+        }
+    }
+}
