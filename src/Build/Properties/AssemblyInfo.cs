5b947ffb900c65dc8006ebd4f9f5a24c961ab2cb
Kevin Obee <kevin.obee@gmail.com>
2016-04-27 16:08:18 +0100

    Add Build.csproj to capture solution dependencies and link to build files

---
+++ src/Build/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Build")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyProduct("Build")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c7d745e0-56f8-4060-9789-64c11ba5cbef")]
